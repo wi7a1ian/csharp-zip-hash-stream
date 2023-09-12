084ec02a23f432b0db8726023af0d456dc1b9797
Wi7a1ian <witalian+github@pm.me>
2023-09-12 12:17:23 +0200

    Tar version

--- src/ZipBlockHash/Program.cs
+++ src/ZipBlockHash/Program.cs
@@ -2,6 +2,8 @@
 using System.IO.Compression;
 using System.Reflection;
 using System.Security.Cryptography;
+using System.Text;
+using ICSharpCode.SharpZipLib.Tar;
 using Polly;
 using Polly.Contrib.WaitAndRetry;
 using Polly.Retry;
@@ -17,42 +19,38 @@ RetryPolicy fileCopyRetryPolicy = Policy
 
 using var hasher = SHA512.Create();
 var workPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
-var lfsPath = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString());
-Directory.CreateDirectory(lfsPath);
 
 {
     const int bufferSize = 8*1024; // i.e 10*1024*1024;
-    using var memoStream =  new MemoryStream(bufferSize);
+    var buffer = new byte[bufferSize];
     using var nafStream = File.Create(Path.Combine(workPath, "out.naf"));
     using var cstream = new CryptoStream(nafStream, hasher, CryptoStreamMode.Write);
-    using var zipArchive = new ZipArchive(cstream, ZipArchiveMode.Create);
-    zipArchive.CreateEntry($"data/", CompressionLevel.SmallestSize);
+    using var tarStream = new TarOutputStream(cstream, Encoding.UTF8);
 
     int i = 0;
     foreach(var finfo in Directory.GetFiles(@"./data", "*.txt").Select( f => new FileInfo(f)))
     {
-        bool useLFS = finfo.Length > bufferSize;
-        string tmpFilePath = Path.Combine(lfsPath, "tmp.buff");
-        Stream tmpStream = useLFS ? File.Open(tmpFilePath, FileMode.Create) : memoStream;
-
-        fileCopyRetryPolicy.Execute( () => {
-            tmpStream.Position = 0;
-            tmpStream.SetLength(0); // zero alloc reset
-            using var fstream = finfo.Open(FileMode.Open, FileAccess.Read);
-            if(i++%7 == 0) throw new IOException("Oh noes!"); // CHAOS MONKEYYY!
-            fstream.CopyTo(tmpStream);
-        });
-
-        var entry = zipArchive.CreateEntry($"data/{finfo.Name}", CompressionLevel.SmallestSize);
-        using var entryStream = entry.Open();
-        tmpStream.Position = 0;
-        tmpStream.CopyTo(entryStream);
-
-        if(useLFS)
+        using(var fstream = finfo.Open(FileMode.Open, FileAccess.Read))
         {
-            tmpStream.Close();
-            File.Delete(tmpFilePath);
+            var entry = TarEntry.CreateTarEntry(finfo.Name);
+            entry.Size = finfo.Length;
+            tarStream.PutNextEntry(entry);
+
+            int bytesRead = 0;
+            do 
+            {
+                bytesRead = fileCopyRetryPolicy.Execute( () => {
+                    if(i++%7 == 0) throw new IOException("Oh noes!"); // CHAOS MONKEYYY!
+                    return fstream.Read(buffer.AsSpan());
+                });
+
+                if (bytesRead > 0) 
+                {
+                    tarStream.Write(buffer, 0, bytesRead);
+                }
+            } while (bytesRead > 0);
         }
+        tarStream.CloseEntry();
     }
 }
 
